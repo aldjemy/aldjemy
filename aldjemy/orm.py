93ff50291b9cd6a6d435b2bf0b0f104a45082df0
Mihail Krivushin <krivushinme@gmail.com>
2011-07-28 00:47:13 +0400

    Explicitly put backref relations on related models for query building simplicity

--- aldjemy/orm.py
+++ aldjemy/orm.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from sqlalchemy import orm
 from django.db.models.fields.related import ForeignKey, OneToOneField
 from django.db import connection
@@ -19,6 +20,8 @@ def prepare_models():
     models = get_django_models()
     sa_models = getattr(Cache, 'models', {})
 
+    backrefs = defaultdict(list)
+
     for model in models:
         name = model._meta.db_table
         table = tables[name]
@@ -38,17 +41,45 @@ def prepare_models():
                 continue
             parent_model = fk.related.parent_model._meta
             p_table = tables[parent_model.db_table]
+            p_name = parent_model.pk.column
             attrs[fk.name] = orm.relationship(
                     sa_models[parent_model.db_table],
                     foreign_keys=[table.c[fk.column]],
-                    primaryjoin=(table.c[fk.column] == p_table.c['id']),
-                    backref=(fk.rel.related_name.lower().strip('+')
-                             if fk.rel.related_name else None)
+                    primaryjoin=(table.c[fk.column] == p_table.c[p_name]),
                     )
+            # Get backref name
+            backref = (fk.rel.related_name.lower().strip('+')
+                       if fk.rel.related_name else None)
+            if not backref:
+                backref = model._meta.object_name.lower()
+                if not isinstance(fk, OneToOneField):
+                    backref = backref  + '_set'
+            # Will generate relationship for other model
+            r_property = orm.relationship(
+                    sa_models[name],
+                    foreign_keys=[table.c[fk.column]],
+                    primaryjoin=(table.c[fk.column] == p_table.c[p_name]),
+                )
+            r_property.key = backref
+            backrefs[parent_model.db_table].append(r_property)
+
         name = model._meta.db_table
         orm.mapper(sa_models[name], table, attrs)
         model.sa = sa_models[name]
+
     Cache.models = sa_models
+
+    for model_name, backref_list in backrefs.iteritems():
+        model = sa_models[model_name]
+        mapper = model._sa_class_manager.mapper
+        for backref in backref_list:
+            if hasattr(model, backref.key):
+                # Dont overwrite existing attrs
+                # So put backref name explicit on django field
+                continue
+            backref.instrument_class(model._sa_class_manager.mapper)
+            backref.parent = mapper
+            backref.init()
 
 
 class class_property(property):
