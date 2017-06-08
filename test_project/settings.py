6b9b34e499b332119e2340db18c7cbd076acdd4e
Luke Plant <L.Plant.98@cantab.net>
2017-06-08 13:41:06 +0300

    Silence error about test1.urls not existing

--- test_project/settings.py
+++ test_project/settings.py
@@ -110,7 +110,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'test1.urls'
+ROOT_URLCONF = 'sample.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
