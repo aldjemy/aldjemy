cc86cf41d099cabab6f470806f9d51d2a1a51568
danielbradburn <daniel@crunchrapps.com>
2017-07-05 16:49:54 +0200

    Removed unused variable in test.sh and the {sqlite} environment from tox.ini

--- test.sh
+++ test.sh
@@ -4,8 +4,6 @@ PWD=`pwd`
 
 cd test_project
 python manage.py test
-# django.contrib.postgres support in django > 1.7
-SUPPORTS_POSTGRES=$(python -c "import django; print(django.VERSION > (1,7,0,'final',0))")
 
 if [ "$PY27_DJANGO17" = "0" ]; then
     while getopts ":p" opt; do
