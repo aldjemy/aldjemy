4397a45e9df559e772941428a91284684d532877
danielbradburn <daniel@crunchrapps.com>
2017-07-05 17:28:16 +0200

    Added suffix to version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setupconf = dict(
     name='aldjemy',
-    version="0.7.0",
+    version="0.7.0.array",
     license='BSD',
     url='https://github.com/Deepwalker/aldjemy/',
     author='Mihail Krivushin',
