8ca10956e017b601bbe670d711f5f518cf273b5b
Marcos A. de S. Silva <chuckymass@gmail.com>
2018-10-12 11:06:35 -0300

    Add support to geoalchemy2 types

--- aldjemy/table.py
+++ aldjemy/table.py
@@ -13,6 +13,11 @@ except ImportError:
 from aldjemy.types import simple, foreign_key, varchar
 from aldjemy import postgres
 
+try:
+    import geolchemy2 as g2
+    is_supported_geoalchemy2 = True
+except:
+    is_supported_geoalchemy2 = False
 
 DATA_TYPES = {
     'AutoField':         simple(types.Integer),
@@ -46,6 +51,9 @@ DATA_TYPES = {
 # Update with dialect specific data types
 DATA_TYPES['ArrayField'] = lambda field: postgres.array_type(DATA_TYPES, field)
 
+if is_suppoted_geoalchemy2:
+    DATA_TYPES['PointField'] = lambda field: g2.types.Geography(geometry_type='POINT', srid=field.srid)
+    DATA_TYPES['LineStringField'] = lambda field: g2.types.Geography(geometry_type='LINESTRING', srid=field.srid)
 
 # Update with user specified data types
 DATA_TYPES.update(getattr(settings, 'ALDJEMY_DATA_TYPES', {}))
