82e1cf59ec697115f11fc9331d6c0c0fc0de0318
Mikhail Krivushin <krivushinme@gmail.com>
2019-10-30 16:43:45 +0400

    0.10.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setupconf = dict(
     name='aldjemy',
-    version="0.10.0",
+    version="0.10.1",
     license='BSD',
     url='https://github.com/Deepwalker/aldjemy/',
     author='Mihail Krivushin',
