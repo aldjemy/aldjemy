bc7f56e7ef026833a2a21f8ecfd5e4ded1297385
Alexandr Artemyev <mogost@gmail.com>
2020-05-22 15:32:40 +0300

    Update settings.py

--- test_project_postgres/test_project_postgres/settings.py
+++ test_project_postgres/test_project_postgres/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "aldjemy",
         "USER": "postgres",
-        "PASSWORD": "",
+        "PASSWORD": "postgres",
         "HOST": "localhost",
         "PORT": "5432",
     },
