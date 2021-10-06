09052c520681b78d753cfe8cff80d60ba61cacee
Nicolas Delaby <nicolas.delaby@infarm.com>
2021-10-06 18:37:37 +0200

    Test

--- test_project_postgres/sample/models.py
+++ test_project_postgres/sample/models.py
@@ -1,6 +1,7 @@
 import django
 from django.contrib.postgres.fields import ArrayField, DateRangeField, JSONField
 from django.db import models
+from django.utils import timezone
 
 
 class TicTacToeBoard(models.Model):
@@ -13,3 +14,11 @@ class JsonModel(models.Model):
 
 class DateRangeModel(models.Model):
     date_range = DateRangeField()
+
+
+class EmailAddress(models.Model):
+    created = models.DateTimeField(default=timezone.now, verbose_name='Created')
+    user = models.ForeignKey('TicTacToeBoard', models.CASCADE, related_name='email_addresses', verbose_name='User')
+    domain = models.CharField(max_length=254, verbose_name='Domain')
+    email = models.EmailField(unique=True, verbose_name='Email')
+    primary = models.BooleanField(default=False, verbose_name='Primary')
