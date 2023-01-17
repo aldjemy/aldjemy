42cae99d4bdf7ad5f30291ea6ace8d7b44d0a060
Ryan Hiebert <ryan@ryanhiebert.com>
2023-01-17 17:03:57 -0600

    Add a setting to force future mode

--- aldjemy/orm.py
+++ aldjemy/orm.py
@@ -9,12 +9,17 @@ from sqlalchemy.orm import registry
 from .core import get_engine
 from .table import generate_tables
 
+SQLALCHEMY_USE_FUTURE = getattr(settings, "ALDJEMY_SQLALCHEMY_USE_FUTURE", None)
+
 
 def get_session(alias="default", recreate=False, **kwargs):
     connection = connections[alias]
     if not hasattr(connection, "sa_session") or recreate:
         engine = get_engine(alias, **kwargs)
-        session = orm.sessionmaker(bind=engine)
+        kwargs = {"bind": engine}
+        if SQLALCHEMY_USE_FUTURE is not None:
+            kwargs['future'] = SQLALCHEMY_USE_FUTURE
+        session = orm.sessionmaker(**kwargs)
         connection.sa_session = session()
     return connection.sa_session
 
